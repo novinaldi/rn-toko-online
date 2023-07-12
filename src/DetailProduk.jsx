0f138fd0d560be94118fd5544b15882d14ccd568
novinaldi <novinaldi1309@gmail.com>
2023-07-13 01:40:06 +0700

    Disain untuk detail produk

--- src/DetailProduk.jsx
+++ src/DetailProduk.jsx
@@ -1,22 +1,27 @@
 import {
+  ActivityIndicator,
   Image,
   ImageBackground,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from 'react-native';
 import React, {useEffect, useState, useCallback} from 'react';
 import {useFocusEffect} from '@react-navigation/native';
 import API_URL from './config';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import FastImage from 'react-native-fast-image';
 
 export default function DetailProduk({route, navigation}) {
   const [loading, setLoading] = useState(false);
   const [idProduk, setIdProduk] = useState();
   const [gambar, setGambar] = useState();
   const [namaProduk, setNamaProduk] = useState();
+  const [harga, setHarga] = useState();
+  const [desc, setDesc] = useState();
 
   const getDetailProduk = async () => {
     fetch(`${API_URL}/product/` + route.params.id, {
@@ -30,6 +35,8 @@ export default function DetailProduk({route, navigation}) {
         // setIdProduk(json.data.id);
         setNamaProduk(json.data.name);
         setGambar(json.data.image);
+        setHarga(json.data.price);
+        setDesc(json.data.description);
         setLoading(false);
       })
       .catch(e => console.log(e));
@@ -42,33 +49,96 @@ export default function DetailProduk({route, navigation}) {
     }, []),
   );
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <ImageBackground
-        source={{uri: `${gambar}`}}
-        style={{
-          width: '100%',
-          height: '100%',
-          flex: 1 / 2,
-          marginTop: 5,
-          elevation: 5,
-        }}
-        resizeMode="stretch"></ImageBackground>
+  return loading ? (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <ActivityIndicator size="large" color="red" />
+    </View>
+  ) : (
+    <View style={styles.container}>
+      <View style={styles.imgProduk}>
+        <FastImage
+          source={{uri: `${gambar}`}}
+          style={{
+            width: '100%',
+            height: '100%',
+            borderRadius: 10,
+          }}
+          resizeMode={FastImage.resizeMode.stretch}
+        />
+      </View>
       <View
         style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: 'blue',
-        }}></View>
-    </SafeAreaView>
+          flex: 2,
+          backgroundColor: 'white',
+          marginTop: 5,
+          elevation: 5,
+        }}>
+        <ScrollView>
+          <Text
+            style={{
+              color: '#862B0D',
+              fontWeight: 'bold',
+              fontSize: 20,
+              marginHorizontal: 10,
+            }}>{`Rp ${harga?.toLocaleString('id-ID') ?? '0'}`}</Text>
+          <Text
+            style={{
+              color: 'black',
+              fontSize: 17,
+              marginHorizontal: 10,
+              fontWeight: 'bold',
+            }}>
+            {namaProduk}
+          </Text>
+          <Text
+            style={{
+              color: '#862B0D',
+              fontSize: 18,
+              marginHorizontal: 10,
+              marginTop: 10,
+              fontWeight: 'bold',
+              borderBottomWidth: 3,
+              borderBottomColor: '#862B0D',
+            }}>
+            Deskripsi Produk
+          </Text>
+          <Text
+            style={{
+              color: 'black',
+              fontSize: 14,
+              marginHorizontal: 10,
+              textAlign: 'justify',
+            }}>
+            {desc}
+          </Text>
+          <TouchableOpacity
+            style={{
+              alignItems: 'center',
+              backgroundColor: '#0B666A',
+              height: 40,
+              justifyContent: 'center',
+              marginHorizontal: 30,
+              marginTop: 20,
+              borderRadius: 15,
+            }}>
+            <Text style={{color: '#fff', fontWeight: 'bold', fontSize: 16}}>
+              Masukkan Ke-Keranjang
+            </Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FDFDFD',
+  },
+  imgProduk: {
+    flex: 1,
+    backgroundColor: 'white',
+    elevation: 5,
   },
 });
--- src/ListProduk.jsx
+++ src/ListProduk.jsx
@@ -1,3 +1,4 @@
+import React, {useState, useCallback} from 'react';
 import {
   StyleSheet,
   Text,
@@ -10,30 +11,33 @@ import {
   Image,
   Alert,
 } from 'react-native';
-import React, {useState, useCallback} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {useFocusEffect} from '@react-navigation/native';
 import API_URL from './config';
 
 export default function ListProduk({navigation}) {
-  const [ListProduk, setListProduk] = useState([]);
+  const [listProduk, setListProduk] = useState([]);
   const [loading, setLoading] = useState(false);
   const [refresh, setRefresh] = useState(false);
 
   const getListProduk = async () => {
-    await fetch(`${API_URL}/product/list`, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-      },
-    })
-      .then(response => response.json())
-      .then(json => {
-        // console.log(json.data);
-        setListProduk(json.data);
-        setLoading(false);
-      })
-      .catch(e => console.log(e.response));
+    try {
+      const response = await fetch(`${API_URL}/product/list`, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+      const json = await response.json();
+      setListProduk(json.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      ToastAndroid.show(
+        'Terjadi kesalahan saat mengambil data',
+        ToastAndroid.SHORT,
+      );
+    }
   };
 
   useFocusEffect(
@@ -43,81 +47,114 @@ export default function ListProduk({navigation}) {
     }, []),
   );
 
-  return loading ? (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <ActivityIndicator size="large" color="blue" />
-    </View>
-  ) : (
-    <SafeAreaView style={{flex: 1}}>
-      <FlatList
-        refreshControl={
-          <RefreshControl
-            refreshing={refresh}
-            onRefresh={() => {
-              getListProduk();
-              setRefresh(false);
-            }}
-          />
-        }
-        data={ListProduk}
-        numColumns={2}
-        contentContainerStyle={{paddingVertical: 10}}
-        keyExtractor={item => item.id}
-        renderItem={({item, index}) => (
-          <TouchableOpacity
-            onPress={() =>
-              navigation.navigate('DetailProduk', {
-                id: item.id,
-                idProduk: item.id,
-              })
+  const renderProdukItem = ({item, index}) => {
+    if (!item) {
+      return null; // Tidak melakukan render jika objek item undefined
+    }
+
+    const navigateToDetail = () => {
+      navigation.navigate('DetailProduk', {
+        id: item.id,
+        idProduk: item.id,
+      });
+    };
+
+    return (
+      <TouchableOpacity
+        onPress={navigateToDetail}
+        style={styles.produkContainer}>
+        <Image
+          source={{
+            uri: `${item.image}`,
+            headers: {
+              Pragma: 'no-cache',
+            },
+          }}
+          style={styles.produkImage}
+        />
+        <Text style={styles.produkName}>
+          {item.name.length > 20
+            ? `${item.name.substring(0, 20)} ...`
+            : item.name}
+        </Text>
+        <Text style={styles.produkPrice}>
+          {`Rp ${item.price?.toLocaleString('id-ID') || '-'}`}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      {loading ? (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="blue" />
+        </View>
+      ) : (
+        <SafeAreaView style={styles.safeAreaContainer}>
+          <FlatList
+            refreshControl={
+              <RefreshControl
+                refreshing={refresh}
+                onRefresh={() => {
+                  setRefresh(true);
+                  getListProduk();
+                  setRefresh(false);
+                }}
+              />
             }
-            style={{
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginVertical: 10,
-              marginHorizontal: 10,
-              borderWidth: 0,
-              borderColor: '#98EECC',
-              elevation: 1,
-            }}>
-            <Image
-              source={{
-                uri: `${item.image}`,
-                headers: {
-                  Pragma: 'no-cache',
-                },
-              }}
-              style={{
-                width: 100,
-                height: 100,
-                resizeMode: 'stretch',
-                marginTop: 3,
-              }}
-            />
-            <Text
-              style={{
-                color: '#071952',
-                fontSize: 14,
-                fontWeight: 'bold',
-              }}>
-              {item.name.length > 20
-                ? `${item.name.substring(0, 20)} ...`
-                : item.name}
-            </Text>
-            <Text
-              style={{
-                paddingBottom: 5,
-                fontWeight: 'bold',
-                color: '#862B0D',
-              }}>
-              {`Rp ${item.price.toLocaleString('id-ID')}`}
-            </Text>
-          </TouchableOpacity>
-        )}
-      />
-    </SafeAreaView>
+            data={listProduk}
+            numColumns={2}
+            contentContainerStyle={styles.flatListContent}
+            keyExtractor={item => item.id.toString()}
+            renderItem={renderProdukItem}
+          />
+        </SafeAreaView>
+      )}
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FDFDFD',
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  safeAreaContainer: {
+    flex: 1,
+  },
+  flatListContent: {
+    paddingVertical: 10,
+  },
+  produkContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10,
+    marginHorizontal: 10,
+    borderWidth: 0,
+    borderColor: '#98EECC',
+    elevation: 1,
+  },
+  produkImage: {
+    width: 100,
+    height: 100,
+    resizeMode: 'stretch',
+    marginTop: 3,
+  },
+  produkName: {
+    color: '#071952',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  produkPrice: {
+    paddingBottom: 5,
+    fontWeight: 'bold',
+    color: '#862B0D',
+  },
+});
