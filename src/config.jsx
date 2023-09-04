780a281761a20dd801f5d189b521dfbbc2120c58
Al Amrikasir <amrikasir@gmail.com>
2023-09-05 04:33:21 +0700

    gunakan hosted api

--- src/config.jsx
+++ src/config.jsx
@@ -1,2 +1,2 @@
-// export default API_URL = 'https://tugas-akhir-tacelak.online/laravel/public/api';
-export default API_URL = 'https://0543-36-69-15-44.ngrok-free.app/api';
+export default API_URL = 'https://tugas-akhir-tacelak.online/laravel/public/api';
+// export default API_URL = 'https://0543-36-69-15-44.ngrok-free.app/api';
